89462c53fcc9cfd95e8fc9e5556e03aebb98d0d6
THUNDERWANG <59207918+THUNDERWANG@users.noreply.github.com>
2020-03-26 10:26:03 -0700

    Version 6

--- CubeCounter.java
+++ CubeCounter.java
@@ -1,3 +1,12 @@
+/** CubeCounter v6
+ * - added StringBuilder class
+ * - cleaned up names of variables and methods
+ * 
+ * How it works:
+ * This program scans multiple card lists, stores the cards as arrays, compares them and prints out results.
+ * Change the String cube_path and String query_path to the appropriate file path
+ */
+
 import java.io.File;
 import java.text.DecimalFormat;
 import java.util.ArrayList;
@@ -8,19 +17,13 @@ import java.util.List;
 import java.lang.StringBuilder;
 import java.util.Arrays;
 
-/* Version 5
-- Split up code into more methods
-- Cleaned up code organization
-- May rename variables later 
+public class CubeCounter_v6 {
 
-*/
-
-public class CubeCounter {
-
-//read cube file and store as ArrayList
+  //read cube file and store cards as ArrayList
   public static List<String> readCube(String cube_path) {
 
     List<String> cube_cards_list = new ArrayList<String> ();
+
     try {
       File cube_file = new File (cube_path);
       Scanner reader =  new Scanner (cube_file);
@@ -31,35 +34,35 @@ public class CubeCounter {
         }
       reader.close();
     }
-    catch (FileNotFoundException exception2) {System.out.println("Some error occured with the cube list upload");  exception2.printStackTrace();
+    catch (FileNotFoundException exception) {System.out.println("Some error occured with the cube list upload");  exception.printStackTrace();
     }
     return cube_cards_list; 
   }
 
-  //read query file and store as Arraylist
+  //read query file and store cards as Arraylist
   public static List<String> readQuery(String query_path) {
 
-    List<String> query_reference_list = new ArrayList<String> ();  
+    List<String> query_cards_list = new ArrayList<String> ();  
     try {
       File query_file = new File (query_path);
       Scanner reader =  new Scanner (query_file);
         while (reader.hasNextLine()) { 
-          String query_data = (reader.nextLine());
-          query_reference_list.add((query_data)); //populate array with what it reads
+          String query_data = reader.nextLine();
+          query_cards_list.add((query_data)); //populate array with what it reads
         }
       reader.close();
     }
     catch (FileNotFoundException exception) {System.out.println("Some error occured with the query upload"); exception.printStackTrace();
     }
-    return query_reference_list;
+    return query_cards_list;
   }
 
-//compare ArrayLists by using loops to compare the elements and record hits to a new Arraylist
-  public static List<String> getHits(List<String> query, List<String> cube) {
+  //compare ArrayLists by using loops to compare the elements and record the hits to a new Arraylist
+  public static List<String> findHits(List<String> query, List<String> cube) {
 
     List<String> hit_cards_list = new ArrayList<String> ();
-    for (int i = 0; i < query.size(); ++i) { 
-      for (int j = cube.size() - 1; j >= 0; --j) { //only going backwards here for practice
+    for (int i = 0; i < query.size(); i++) { 
+      for (int j = cube.size() - 1; j >= 0; j--) { //only going backwards here for practice
         if (query.get(i).equalsIgnoreCase(cube.get(j) ) ) {
         hit_cards_list.add(cube.get(j));
         }
@@ -68,31 +71,30 @@ public class CubeCounter {
     return hit_cards_list;
   }
 
-
   public static String calculateToString(String query_name, List<String> query, List<String> cube) {
 
-    List<String> hitListHereJustToGetAroundScopeProblem = getHits(query, cube);
+    List<String> hit_cards_list = findHits(query, cube);
 
     //Format data to look pretty
-    Collections.sort(hitListHereJustToGetAroundScopeProblem);
-    double hit_percentage = (double) hitListHereJustToGetAroundScopeProblem.size() /cube.size() * 100.0;
+    Collections.sort(hit_cards_list);
+    double hit_percentage = (double) hit_cards_list.size() /cube.size() * 100.0;
     String hit_percentage_formatted = String.format("%.2f", hit_percentage);
 
-    StringBuilder stringy = new StringBuilder();
-    for (String hits : hitListHereJustToGetAroundScopeProblem) {
-      stringy.append("Match: " ).append(hits).append("\n");
+    StringBuilder results_string = new StringBuilder();
+    for (String hits : hit_cards_list) {
+      results_string.append("Match: " ).append(hits).append("\n");
     }
-    stringy.append("\n").append("Total cards in cube: ").append(cube.size())
-    .append("\n").append("Number of ").append(query_name).append(" ").append(hitListHereJustToGetAroundScopeProblem.size())
-    .append("\n").append("Number of ").append(query_name).append(" cards found: ").append (hitListHereJustToGetAroundScopeProblem.size())
-    .append("\n").append(query_name).append(" in cube: ").append(hit_percentage_formatted).append("%");
+    results_string.append("\nTotal cards in cube: ").append(cube.size())
+    .append("\nNumber of ").append(query_name).append(" cards found: ").append(hit_cards_list.size()).append(" | ")
+    .append(hit_percentage_formatted).append("%");
     
-    return stringy.toString();    
-  }
+    return results_string.toString();    
+  } 
   
     public static void main(String[] args) {
-      String cube_path = "C:\\Users\\Jason\\Desktop\\THUNDER.txt";
-      String query_path = "C:\\Users\\Jason\\Desktop\\Master.txt";
+      String parent_path = "C:\\Users\\Jason\\Desktop\\";
+      String cube_path = parent_path + "THUNDER" + ".txt";
+      String query_path = parent_path + "Master" + ".txt";
       String query_name = "Removal";
 
       List<String> query = readQuery(query_path);
