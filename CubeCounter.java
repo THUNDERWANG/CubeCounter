c95eeb58f9e796ff9f3b48ee0e9fbca5663c0829
THUNDERWANG <59207918+THUNDERWANG@users.noreply.github.com>
2020-05-02 16:49:32 -0700

    Big Update
    Finds the ASFAN of cubes and writes the results to a CSV file.

--- CubeCounter.java
+++ CubeCounter.java
@@ -1,106 +1,347 @@
-/** CubeCounter v6
- * - added StringBuilder class
- * - cleaned up names of variables and methods
- * 
- * How it works:
- * This program scans multiple card lists, stores the cards as arrays, compares them and prints out results.
- * Change the String cube_path and String query_path to the appropriate file path
- */
-
 import java.io.File;
+import java.io.FileFilter;
 import java.text.DecimalFormat;
-import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.Collections; 
-import java.io.FileNotFoundException;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
 import java.lang.StringBuilder;
-import java.util.Arrays;
+import java.math.RoundingMode;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.HashMap;
+import java.text.DecimalFormat;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.text.NumberFormat;
 
-public class CubeCounter {
+public class Cube {
 
-  //read cube file and store cards as ArrayList
-  public static List<String> readCube(String cube_path) {
+    private String name;
+    private String filePath;
+    private int size;
+    private ArrayList<String> cardArray;
+    private ArrayList<String> artifactArray;
+    private ArrayList<String> bounceArray;
+    private ArrayList<String> countersArray;
+    private ArrayList<String> disruptionArray;
+    private ArrayList<String> landArray;
+    private ArrayList<String> massRemovalArray;
+    private ArrayList<String> removalArray;
+    private ArrayList<String> walkersArray;
+    private ArrayList<String> duplicates;
+    private HashMap<String, Double> stats;
 
-    List<String> cube_cards_list = new ArrayList<String> ();
+    public Cube(String name, String filePath) {
+        this.name = name;
+        this.filePath = filePath;
+        this.cardArray = pathToArray(filePath);
+        this.size = cardArray.size();
+    }
 
-    try {
-      File cube_file = new File (cube_path);
-      Scanner reader =  new Scanner (cube_file);
-        
-        while (reader.hasNextLine()) {
-        String cube_data = reader.nextLine(); 
-        cube_cards_list.add(cube_data);
+    public static ArrayList<String> pathToArray(String filePath) {
+        File file = new File(filePath);
+        ArrayList<String> cardArray = new ArrayList<String>();
+        try {
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String card = scanner.nextLine();
+                if (!card.isBlank()) {
+                    String parsedCard = card.replace(",", "|");
+                    cardArray.add(parsedCard);
+                }
+
+            }
+            scanner.close();
+        } catch (Exception exception) {
+            System.out.println(exception);
         }
-      reader.close();
+        Collections.sort(cardArray);
+        return cardArray;
     }
-    catch (FileNotFoundException exception) {System.out.println("Some error occured with the cube list upload");  exception.printStackTrace();
-    }
-    return cube_cards_list; 
-  }
 
-  //read query file and store cards as Arraylist
-  public static List<String> readQuery(String query_path) {
-
-    List<String> query_cards_list = new ArrayList<String> ();  
-    try {
-      File query_file = new File (query_path);
-      Scanner reader =  new Scanner (query_file);
-        while (reader.hasNextLine()) { 
-          String query_data = reader.nextLine();
-          query_cards_list.add((query_data)); //populate array with what it reads
+    public static ArrayList<String> loadCheck(String filePath) {
+        ArrayList<String> cardArray = pathToArray(filePath);
+        ArrayList duplicates = checkDuplicates(cardArray);
+        if (!duplicates.isEmpty()) {
+            System.out.println("Duplicate Detetected: " + duplicates + " in " + filePath);
         }
-      reader.close();
+        return cardArray;
     }
-    catch (FileNotFoundException exception) {System.out.println("Some error occured with the query upload"); exception.printStackTrace();
-    }
-    return query_cards_list;
-  }
 
-  //compare ArrayLists by using loops to compare the elements and record the hits to a new Arraylist
-  public static List<String> findHits(List<String> query, List<String> cube) {
-
-    List<String> hit_cards_list = new ArrayList<String> ();
-    for (int i = 0; i < query.size(); i++) { 
-      for (int j = cube.size() - 1; j >= 0; j--) { //only going backwards here for practice
-        if (query.get(i).equalsIgnoreCase(cube.get(j) ) ) {
-        hit_cards_list.add(cube.get(j));
+    public static ArrayList<String> checkDuplicates(ArrayList<String> cardArray) {
+        HashSet<String> singleSet = new HashSet<String>();
+        ArrayList<String> duplicateArray = new ArrayList<String>();
+        for (String card : cardArray) {
+            if (!singleSet.add(card)) {
+                duplicateArray.add(card);
+            }
         }
-      }
+
+        return duplicateArray;
     }
-    return hit_cards_list;
-  }
 
-  public static String calculateToString(String query_name, List<String> query, List<String> cube) {
-
-    List<String> hit_cards_list = findHits(query, cube);
-
-    //Format data to look pretty
-    Collections.sort(hit_cards_list);
-    double hit_percentage = (double) hit_cards_list.size() /cube.size() * 100.0;
-    String hit_percentage_formatted = String.format("%.2f", hit_percentage);
-
-    StringBuilder results_string = new StringBuilder();
-    for (String hits : hit_cards_list) {
-      results_string.append("Match: " ).append(hits).append("\n");
+    public static ArrayList<String> findHits(Cube cube, ArrayList<String> master) {
+        var hitList = new ArrayList<String>();
+        for (String card : cube.cardArray) {
+            if (master.contains(card)) {
+                hitList.add(card);
+            }
+        }
+        Collections.sort(hitList);
+        return hitList;
     }
-    results_string.append("\nTotal cards in cube: ").append(cube.size())
-    .append("\nNumber of ").append(query_name).append(" cards found: ").append(hit_cards_list.size()).append(" | ")
-    .append(hit_percentage_formatted).append("%");
-    
-    return results_string.toString();    
-  } 
-  
+
+    // just loops findHits method for each cube in cubeList Array
+    public static void findHitsAll(ArrayList<Cube> cubeList, Cube master) {
+        for (Cube cube : cubeList) {
+            cube.artifactArray = findHits(cube, master.artifactArray);
+            cube.bounceArray = findHits(cube, master.bounceArray);
+            cube.countersArray = findHits(cube, master.countersArray);
+            cube.disruptionArray = findHits(cube, master.disruptionArray);
+            cube.landArray = findHits(cube, master.landArray);
+            cube.removalArray = findHits(cube, master.removalArray);
+            cube.walkersArray = findHits(cube, master.walkersArray);
+            cube.massRemovalArray = findHits(cube, master.massRemovalArray);
+
+        }
+    }
+
+    public static Double calculate(Cube cube, ArrayList<String> hitArray) {
+        double unformatPercent = ((double) hitArray.size()) / cube.size * 100.0;
+        DecimalFormat formatter = new DecimalFormat("##.##");
+        Double percent = Double.valueOf(formatter.format(unformatPercent));
+        return percent;
+    }
+
+    // just loops calculate method for each cube in cubeList Array
+    public static void calculateAll(ArrayList<Cube> cubeList) {
+        for (Cube cube : cubeList) {
+            cube.stats = new HashMap<String, Double>();
+            cube.stats.put("Artifact-Fixing", calculate(cube, cube.artifactArray));
+            cube.stats.put("Bounce", calculate(cube, cube.bounceArray));
+            cube.stats.put("Counters", calculate(cube, cube.countersArray));
+            cube.stats.put("Disruption (hand)", calculate(cube, cube.disruptionArray));
+            cube.stats.put("Land-Fixing", calculate(cube, cube.landArray));
+            cube.stats.put("Removal", calculate(cube, cube.removalArray));
+            cube.stats.put("Walkers", calculate(cube, cube.walkersArray));
+            cube.stats.put("Mass Removal", calculate(cube, cube.massRemovalArray));
+
+        }
+    }
+
     public static void main(String[] args) {
-      String parent_path = "C:\\Users\\Jason\\Desktop\\";
-      String cube_path = parent_path + "THUNDER" + ".txt";
-      String query_path = parent_path + "Master" + ".txt";
-      String query_name = "Removal";
 
-      List<String> query = readQuery(query_path);
-      List<String> cube = readCube(cube_path);
-      String results = calculateToString(query_name, query, cube);
-      System.out.println(results);
-    
+        ArrayList<Cube> cubeList = new ArrayList<Cube>();
+
+        // change the directory C:\\Users\\Jason\\Desktop\\ if needed:
+        String parentPath = "C:\\Users\\Jason\\Desktop\\Cube Project\\";
+
+        Cube master = new Cube("master", parentPath + "Master Lists\\Master.txt");
+        master.artifactArray = loadCheck(parentPath + "Master Lists\\Artifact Fixing.txt");
+        master.bounceArray = loadCheck(parentPath + "Master Lists\\Bounce.txt");
+        master.countersArray = loadCheck(parentPath + "Master Lists\\Counters.txt");
+        master.disruptionArray = loadCheck(parentPath + "Master Lists\\Discard.txt");
+        master.landArray = loadCheck(parentPath + "Master Lists\\Land Fixing.txt");
+        master.removalArray = loadCheck(parentPath + "Master Lists\\Removal.txt");
+        master.walkersArray = loadCheck(parentPath + "Master Lists\\Walkers.txt");
+        master.massRemovalArray = loadCheck(parentPath + "Master Lists\\Mass Removal.txt");
+
+        // create cube objects here:
+        Cube thunder = new Cube("THUNDERWANG", parentPath + "Cube Lists\\THUNDERDOMEXMAGELIST.txt");
+        cubeList.add(thunder);
+        Cube ikzann = new Cube("ikzann", parentPath + "Cube Lists\\Reasonablecube.txt");
+        cubeList.add(ikzann);
+        Cube steveMan = new Cube("The_Steve_Man", parentPath + "Cube Lists\\540_high_octane_cube.txt");
+        cubeList.add(steveMan);
+        Cube lemem = new Cube("Lemem", parentPath + "Cube Lists\\Lemems450UnderpoweredCube.txt");
+        cubeList.add(lemem);
+        Cube kitsuneCurator = new Cube("KitsuneCurator", parentPath + "Cube Lists\\400LegacyCube.txt");
+        cubeList.add(kitsuneCurator);
+        Cube steveIce = new Cube("steve_ice", parentPath + "Cube Lists\\LessAggressive360Fair.txt");
+        cubeList.add(steveIce);
+        Cube cbs2018 = new Cube("cbs2018", parentPath + "Cube Lists\\BrawlStandardV4.txt");
+        cubeList.add(cbs2018);
+        Cube suri = new Cube("suri", parentPath + "\\Cube Lists\\SurisCube.txt");
+        cubeList.add(suri);
+        Cube visserdrix = new Cube("Visserdrix", parentPath + "\\Cube Lists\\VisserdrixCube.txt");
+        cubeList.add(visserdrix);
+        Cube domcosta = new Cube("domcosta", parentPath + "\\Cube Lists\\domcosta.txt");
+        cubeList.add(domcosta);
+        Cube mapcaster = new Cube("Mapcaster Snage [Vintage]", parentPath + "\\Cube Lists\\540VintageUnpowered.txt");
+        cubeList.add(mapcaster);
+        Cube mapcaster2 = new Cube("Mapcaster Snage [Fair]", parentPath + "\\Cube Lists\\360LegacyFair.txt");
+        cubeList.add(mapcaster2);
+        Cube strider212 = new Cube("Strider212", parentPath + "\\Cube Lists\\Strider212sUnpoweredModernishCube.txt");
+        cubeList.add(strider212);
+        Cube schweinefett = new Cube("schweinefett", parentPath + "\\Cube Lists\\Actual385cubeoldbordered.txt");
+        cubeList.add(schweinefett);
+        Cube indigoZach = new Cube("indigo.zach", parentPath + "\\Cube Lists\\450Peasant.txt");
+        cubeList.add(indigoZach);
+        Cube thesidestepkids = new Cube("thesidestepkids", parentPath + "\\Cube Lists\\360Unpowered.txt");
+        cubeList.add(thesidestepkids);
+        Cube cygnetFreud = new Cube("CygnetFreud", parentPath + "\\Cube Lists\\NicksCube.txt");
+        cubeList.add(cygnetFreud);
+        Cube tjornan = new Cube("Tjornan", parentPath + "\\Cube Lists\\450_unpowered.txt");
+        cubeList.add(tjornan);
+        Cube strictlycheese = new Cube("strictlycheese", parentPath + "\\Cube Lists\\cube_of_cheese.txt");
+        cubeList.add(strictlycheese);
+
+        // loop findHits and calculateAll method for each cube
+        findHitsAll(cubeList, master);
+        calculateAll(cubeList);
+        
+        LocalDateTime date = LocalDateTime.now();
+        DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd-MMM-yyyy");
+        String dateStamp = date.format(dateFormatter);
+
+        //write the results of each cube to ONE .csv file
+        try {
+            FileWriter fileWriter = new FileWriter(parentPath + "\\Results\\Results " + dateStamp + ".csv");
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            PrintWriter printWriter = new PrintWriter(bufferedWriter);
+
+            //create column names
+            printWriter.println(
+                dateStamp + "\n" 
+                + "Name" + "," 
+                + "Size " + "," 
+                + "Removal %" + "," 
+                + "Mass Removal %" + ","
+                + "Land-Fixing %" + "," 
+                + "Artifact-Fixing %" + "," 
+                + "Walker %" + "," 
+                + "Bounce %" + ","
+                + "Counters %" + "," 
+                + "Disruption (hand) %" + "," 
+                + "," 
+                + "Removal/draft" + "," 
+                + "Mass Removal/draft" + ","
+                + "Land-Fixing/draft" + "," 
+                + "Artifact-Fixing/draft" + "," 
+                + "Walker/draft" + "," 
+                + "Bounce/draft" + ","
+                + "Counters/draft" + "," 
+                + "Disruption (hand)/draft");
+
+            NumberFormat numf = new DecimalFormat ("##.##");
+            final double perDraft = (360.0/100.0); 
+
+            ///create the records (rows of information)
+            for (Cube cube : cubeList) {
+                printWriter.print(
+                    cube.name + "," 
+                    + cube.size + "," 
+                    + cube.stats.get("Removal") + ","
+                    + cube.stats.get("Mass Removal") + "," 
+                    + cube.stats.get("Land-Fixing") + ","
+                    + cube.stats.get("Artifact-Fixing") + "," 
+                    + cube.stats.get("Walkers") + ","
+                    + cube.stats.get("Bounce") + "," 
+                    + cube.stats.get("Counters") + ","
+                    + cube.stats.get("Disruption (hand)") + "," 
+                    + "," 
+                    + numf.format(cube.stats.get("Removal")*perDraft) + "," 
+                    + numf.format(cube.stats.get("Mass Removal")*perDraft) + "," 
+                    + numf.format(cube.stats.get("Land-Fixing")*perDraft) + ","
+                    + numf.format(cube.stats.get("Artifact-Fixing")*perDraft) + "," 
+                    + numf.format(cube.stats.get("Walkers")*perDraft) + ","
+                    + numf.format(cube.stats.get("Bounce")*perDraft) + "," 
+                    + numf.format(cube.stats.get("Counters")*perDraft) + ","
+                    + numf.format(cube.stats.get("Disruption (hand)")*perDraft)
+                    + "\n");
+                printWriter.flush();
+                }  
+            printWriter.close();
+               
+        } catch (
+            Exception exception) {
+            System.out.println(exception);
+        }
+
+        //write all found cards to SEPARATE cube csv files
+        try {
+            for (Cube cube : cubeList) {
+                FileWriter fileWriter = new FileWriter(parentPath + "\\Results\\" + cube.name + " " + dateStamp+ ".csv");
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                PrintWriter printWriter = new PrintWriter(bufferedWriter);
+                
+                //create column names
+                printWriter.println(
+                    cube.name + "," 
+                    + dateStamp + "\n" 
+                    + "Removal" + "," 
+                    + "Mass Removal" + "," 
+                    + "Land-Fixing" + "," 
+                    + "Artifact-Fixing" + "," 
+                    + "Walkers" + "," 
+                    + "Bounce" + "," 
+                    + "Counters" + "," 
+                    + "Disruption (hand)");
+
+                for (int i = 0; i<150; i++ ) {
+
+                    /*150 is just some arbitrary number. Technically it should be based off
+                    of the array with the highest number of cards in them.
+                    I'm pretty sure there's a better way to control the loop.
+                    I might add all the arrays into another arraylist and loop through from there
+                    to clean up the code*/
+
+                    if (i < cube.removalArray.size()) {
+                        printWriter.print(cube.removalArray.get(i));
+                    } else {
+                        printWriter.print("");
+                    }
+                    if (i < cube.massRemovalArray.size()) {
+                        printWriter.print("," + cube.massRemovalArray.get(i));
+                    } else {
+                        printWriter.print("," + "");
+                    }
+                    if (i < cube.landArray.size()) {
+                        printWriter.print("," + cube.landArray.get(i));
+                    } else {
+                        printWriter.print("," + "");
+                    }
+                    if (i < cube.artifactArray.size()) {
+                        printWriter.print("," + cube.artifactArray.get(i));
+                    } else {
+                        printWriter.print("," + "");
+                    }
+                    if (i < cube.walkersArray.size()) {
+                        printWriter.print("," + cube.walkersArray.get(i));
+                    } else {
+                        printWriter.print("," + "");
+                    }
+                    if (i < cube.bounceArray.size()) {
+                        printWriter.print("," + cube.bounceArray.get(i));
+                    } else {
+                        printWriter.print("," + "");
+                    }
+                    if (i < cube.countersArray.size()) {
+                        printWriter.print("," + cube.countersArray.get(i));
+                    } else {
+                        printWriter.print("," + "");
+                    }
+                    if (i < cube.disruptionArray.size()) {
+                        printWriter.print("," + cube.disruptionArray.get(i));
+                    } else {
+                        printWriter.print("," + "");
+                    }
+                    printWriter.println();
+                    printWriter.flush();
+                }
+                printWriter.close();
+
+            }
+
+        } catch (
+            Exception exception) {
+            System.out.println(exception);
+        }
     }
 }
